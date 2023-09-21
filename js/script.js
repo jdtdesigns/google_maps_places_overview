507fa71321ec93947a4dd64a3d28e1aa2d3b38ea
JD Tadlock <jt7903@gmail.com>
2023-09-21 10:00:33 -0400

    finished maps overview

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// AIzaSyB_9_LFP6G6wvFaO7nNQOKBmUpPrq_v9o8
+
+var map;
+var service;
+
+function generateMapMarkers(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+}
+
+function getHikingTrails() {
+  var request = {
+    location: map.getCenter(),
+    radius: '5000',
+    query: 'hiking trails'
+  };
+
+  service.textSearch(request, generateMapMarkers);
+}
+
+function getLocation(callback) {
+  if (navigator) {
+    navigator.geolocation.getCurrentPosition(function (data) {
+      callback(data.coords.latitude, data.coords.longitude);
+    })
+  }
+}
+
+
+function initMap() {
+  // Set the location (City or Latitude/Longitude)
+  getLocation(function (lat, long) {
+    var location = { lat: lat, lng: long };
+
+    // Create a new Map instance
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: location,
+      zoom: 12
+    });
+
+    // Create a new PlacesService instance
+    service = new google.maps.places.PlacesService(map);
+
+    getHikingTrails();
+  });
+}
+
